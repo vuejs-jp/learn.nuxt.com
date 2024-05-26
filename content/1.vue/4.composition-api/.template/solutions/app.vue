3eeb27db55fb97136779781273eebacdaed57358
shiyuu <tamemotoyou@gmail.com>
2024-05-26 16:35:24 +0900

    feat(wip): add playground file

---
+++ content/1.vue/4.composition-api/.template/solutions/app.vue
@@ -0,0 +1,13 @@
+<script setup>
+const { count, doubled, increment } = useCounter()
+</script>
+
+<template>
+  <div>
+    <p>count is {{ count }}</p>
+    <p>doubled is {{ doubled }}</p>
+    <button @click="increment">
+      +1
+    </button>
+  </div>
+</template>
