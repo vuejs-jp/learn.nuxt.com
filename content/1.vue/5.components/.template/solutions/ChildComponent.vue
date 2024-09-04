82339e26ddf096feef3c9d73544cadde42dd13ab
aster-mnch <mnch.mai@gmail.com>
2024-09-04 23:43:28 +0900

    feat: content/1.vue/5.components (#7)

---
+++ content/1.vue/5.components/.template/solutions/ChildComponent.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+defineProps<{
+  message: string
+}>()
+
+const name = defineModel<string>()
+</script>
+
+<template>
+  <div class="child-component">
+    <h2>Child Component</h2>
+    <p>{{ message }}</p>
+    <input
+      v-model="name"
+      type="text"
+    >
+  </div>
+</template>
+
+<style scoped>
+.child-component {
+  border: solid red;
+  padding: 1rem;
+}
+</style>
