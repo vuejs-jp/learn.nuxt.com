a30511e86ca4e2025535971c50b4e6bfee85667f
ubugeeei <71201308+Ubugeeei@users.noreply.github.com>
2024-09-06 21:13:09 +0900

    feat: content/1.vue/5.components (#65)
    * chore: word
    
    * feat: 1.vue/5.components

--- content/1.vue/5.components/.template/solutions/app.vue
+++ content/1.vue/5.components/.template/solutions/app.vue
@@ -1,16 +1,30 @@
 <script setup lang="ts">
-import ChildComponent from './ChildComponent.vue'
+import Child from './Child.vue'
 
 const name = ref('John Doe')
+
+function updateName(value: string) {
+  name.value = value
+}
 </script>
 
 <template>
   <div>
     <h1>Parent Component</h1>
     <p>Hi, {{ name }} 👋</p>
-    <ChildComponent
-      v-model="name"
-      message="Hello from Parent!"
-    />
+    <Child
+      :name
+      @update:name="updateName"
+    >
+      <template #paragraph>
+        Hello from <span class="red--text">Parent!</span>
+      </template>
+    </Child>
   </div>
 </template>
+
+<style scoped>
+.red--text {
+  color: red;
+}
+</style>
